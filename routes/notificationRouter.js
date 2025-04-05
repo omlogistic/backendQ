a73e7b1b88070c455ccb6b9ed43053ddbe9c1fdb
omlogistic <ankit.pandey@omlogistics.co.in>
2025-04-05 09:28:12 +0530

    chnages in routes

--- routes/notificationRouter.js
+++ routes/notificationRouter.js
@@ -5,6 +5,6 @@ const { sendBookingEmail } = require("../controllers/notificationController");
 const notificationRouter = express.Router();
 
 // Define the route for booking confirmation
-notificationRouter.post("/book", sendBookingEmail);
+notificationRouter.post("/booking-email", sendBookingEmail);
 
 module.exports = notificationRouter;
